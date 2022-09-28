e83d0993ac9e955be64a89d5799fb2024d5b544c
Amit <jaykabdirect@gmail.com>
2022-09-28 12:01:28 +0100

    Java fundamentals

---
+++ src/com/qa/javafundamentals/OperatorTest.java
@@ -0,0 +1,20 @@
+package com.qa.javafundamentals;
+
+public class OperatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int salary; 
+		salary = 30000;
+		
+		float productPrice;
+		productPrice = 450.89f;
+		
+		salary = salary +(salary * 10 / 100);
+		System.out.println(salary);
+	
+		
+		
+	}
+
+}
